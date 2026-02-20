3a3e4bfe57d9e6de9904268694669ae079133dc1
lokesh7385 <lokeshuike7385@gmail.com>
2026-02-20 08:54:01 +0530

    .

--- api/index.py
+++ api/index.py
@@ -232,5 +232,5 @@ def upload_route():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5500))
     app.run(host='0.0.0.0', port=port)
